17ea39294849cb1e56ab954ca585433145ff384d
Rahul Varshney <76695044+RahulV-24@users.noreply.github.com>
2023-07-18 18:10:21 +0530

    Update SPI_BIST.v

--- SPI_BIST.v
+++ SPI_BIST.v
@@ -1,169 +1,134 @@
 module BIST(
     input wire clk,
-    //input wire in_data,
      input wire reset, 
-
-    output wire S_cs, 
-    output wire  S_s_clk,
-    output wire[7:0] data_out,
+     input wire master_writeread,
+    input wire [7:0] MOSI_user_input,
+    input wire [7:0] MISO_user_input,
+    input wire select,
+    output wire CS, 
+    output wire  S_clk,
     output wire [3:0] counter,
-    output wire [2:0] Code,
-    output wire [7:0] L_data
+    output wire MOSI_res,
+    output wire MISO_res,
+    output wire [7:0] BIST_output
 );
-//wire [7:0] L_data;
 
-LFSR V0(clk, reset, L_data);
-spi_master V1 (L_data,reset, clk, S_cs, S_s_clk, data_out, counter);
-Cmp V2 (S_s_clk, L_data, data_out, Code);
+wire   MOSI_data_out;
+wire  MISO_data_out;
+wire [7:0] MOSI_data;
+wire [7:0] MISO_data;
 
+wire [7:0] BIST_mode_mux_out;
+
+wire [4:0] count;
+wire [7:0] MOSI_out;   ///MUX input MOSI side
+wire [7:0] MISO_out;   //MUX input MISO side
+wire [7:0] LFSR_out;
+
+BIST_MUX M1(MOSI_user_input,LFSR_out,select,MOSI_out);
+BIST_MUX M2(MISO_user_input,LFSR_out,select,MISO_out);
+
+LFSR V0(clk, reset,LFSR_out,count);
+spi_master V1 (MOSI_out,MISO_data_out,reset, clk, master_writeread, CS, S_clk, MOSI_data_out, counter,MISO_data);
+spi_slave V2 (clk,reset,MOSI_data_out,master_writeread,MISO_out,MISO_data_out,MOSI_data);
+Cmp V3 (clk,select,count,LFSR_out,MOSI_data,LFSR_out,MISO_data,MOSI_res,MISO_res);
+
+assign BIST_output = LFSR_out;
 
 
 endmodule
-
-
-
-
 
 
 module LFSR(
     input wire clk,
     input wire reset,
-    output wire[7:0] data_out
+    output wire[7:0] data_out,
+    output wire[4:0] count
 );
-reg [7:0] out = 8'h00;
-reg [2:0] count=3'd7;
-wire w_xor;
-assign w_xor = out[7] ^ out[1];
+reg [7:0] shiftreg = 8'h00;
+reg xor1,xor2,xor3;
+reg [4:0] counter;
 
 always @(posedge clk)
 begin
+
     if(reset)
         begin
-            out <= 8'h10;
+            shiftreg <= 8'hF0;
+            counter <=4'd9;
         end
     else
-   // while (count>=0) 
-     //   begin
-       //   out[count]<= out[count-1];
-         // count <= count-1;
-         // if(count==0)
-          //  out[count] <= w_xor; 
-        //end 
-        
-        
-        out <= {out[6],out[5],out[4],out[3],out[2],out[1],out[0],w_xor}; 
-        #42;
+         begin
+         if(counter == 0)
+            begin
+            xor1 = shiftreg[7]^shiftreg[5];
+            xor2 = xor1^shiftreg[4];
+            xor3 = xor2^shiftreg[3];
+            shiftreg = {shiftreg[6:0],xor3};
+            counter<=4'd9;
+            end
+         end
+         counter = counter-1;
 end
-assign data_out = out;
+
+assign data_out = shiftreg;
+assign count = counter;
 
 endmodule
-//SPI Module
 
-module spi_master(
 
-    input wire [7:0] in_data,
-    input wire reset,
-    input wire clk,        //system clock
-    //input reset_n;
-    //input go;
-
-    output wire S_cs, 
-    output wire  S_s_clk,
-    output wire  [7:0] data_out,
-    output wire [3:0] counter
-    
-);
-
-//reg BIST_MODE = 0;
-//reg GO;
-reg [7:0] MOSI;   //shift register
-reg cs;            // control chip select   when cs is low(i.e., 0)it transfers data to that slave
-reg s_clk;          // spi clock
-reg [1:0] state;
-reg [3:0] count;
-
-//for normal mode and BIST mode....if BIST MODE==1 then BIST mode is on
-//always @(reset_n) 
-//begin
-    //if(reset_n==1)
-     //   BIST_MODE = 1;
-   // else 
-  //      BIST_MODE = 0;
-    
-//end
-
-//always @(go) 
-//begin
- //   if(go==1)
-
-    
-//end 
-
-always @(posedge clk or posedge reset) 
-begin
-    if(reset)
-        begin
-            MOSI <= 8'b0;
-            count <= 4'd0;
-            cs <= 1'b1;
-            s_clk <= 1'b0;
-        end
-      
-else 
-    begin
-      case(state)
-        0: begin
-          s_clk <= 1'b0;
-          cs <=1'b1;
-          state <=1;
-        end
-
-        1: begin
-          s_clk <= 1'b0;
-          cs <= 1'b0;
-          MOSI <= {MOSI[6:0],in_data};
-          count <= count-1;
-          state <=2;
-        end 
-
-        2:begin
-          s_clk <=1'b1;
-            if (count>0)
-                state <= 1;
-            else begin
-                count <= 8;
-                state <=0;
-                end
-        end
-        default :state <=0; 
-      endcase
-end
-end
-assign S_cs = cs;
-assign S_s_clk = s_clk;
-assign data_out = MOSI;
-assign counter = count;
-
-endmodule   
 
 module Cmp(
     input wire clk,
-    input wire [7:0] L_data,
-    input wire [7:0] S_data,
+    input wire select,
+    input wire [4:0] count,
+    input wire [7:0] MOSI_in_data,
+    input wire [7:0] MOSI_out_data,
+    input wire [7:0] MISO_in_data,
+    input wire [7:0] MISO_out_data,
 
-    output wire [2:0] Code
+    output wire  MOSI_result,
+    output wire  MISO_result
 );
 
-reg [2:0] code;
+reg MOSI_res;
+reg MISO_res;
+    reg [7:0] MOSI_in_data_reg;
+    reg [7:0] MOSI_out_data_reg;
+    reg  [7:0] MISO_in_data_reg;
+    reg  [7:0] MISO_out_data_reg;
 
-always @(negedge clk)
+always @(posedge clk)
+begin
+    if(select == 1)
+  begin
+    if(count == 0)
     begin
-      if(L_data == S_data)
-             code = 3'b101;
-        else
-             code = 3'b001;
+        MOSI_in_data_reg = MOSI_in_data;
+        MISO_in_data_reg = MISO_in_data;
+        
     end
-
-    assign Code = code;
-
+ 
+    if(count == 9)
+    begin
+        MOSI_out_data_reg = MOSI_out_data;
+        MISO_out_data_reg = MISO_out_data;
+        
+    end  
+    
+      if(MOSI_in_data_reg == MOSI_out_data_reg)
+             MOSI_res=1'd1;
+        else
+             MOSI_res = 1'd0;
+             
+       if(MISO_in_data_reg == MISO_out_data_reg)
+             MISO_res=1'd1;
+        else
+             MISO_res = 1'd0;
+    end
+end
+    assign MISO_result  = MISO_res;
+    assign MOSI_result = MOSI_res;
+  
 endmodule
+
