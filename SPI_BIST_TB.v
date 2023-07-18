7bc4cf7a524eef55fff890483d638a3dcef0778c
Rahul Varshney <76695044+RahulV-24@users.noreply.github.com>
2023-07-18 18:13:02 +0530

    Update SPI_BIST_TB.v

--- SPI_BIST_TB.v
+++ SPI_BIST_TB.v
@@ -1,33 +1,44 @@
-
 `include "SPI_BIST.v"
 
 module BIST_tb;
 
     reg clk;
     reg reset; 
-
-    wire S_cs; 
-    wire S_s_clk;
-    wire[7:0] data_out;
+    reg master_writeread;
+    reg [7:0] MOSI_user_input;
+    reg[7:0] MISO_user_input;
+    reg select;
+    
+    wire CS; 
+    wire S_clk;
     wire [3:0] counter;
-    wire [2:0] Code;
-    wire [7:0] L_data;
+    wire MOSI_res;
+    wire MISO_res; 
+    wire [7:0] BIST_output;
 
 BIST dut(
   .clk(clk),
   .reset(reset),
-  .S_cs(S_cs),
-  .S_s_clk(S_s_clk),
-  .data_out(data_out),
+  .master_writeread(master_writeread),
+  .MOSI_user_input(MOSI_user_input),
+  .MISO_user_input(MISO_user_input),
+  .select(select),
+  .CS(CS),
+  .S_clk(S_clk),
   .counter(counter),
-  .Code(Code),
-  .L_data(L_data)
+  .MOSI_res(MOSI_res),
+  .MISO_res(MISO_res),
+  .BIST_output(BIST_output)
 );
 
 initial 
   begin
   clk = 0;
   reset = 1;
+  master_writeread=1;
+  select = 1;
+  MOSI_user_input = 8'hf0;
+  MISO_user_input = 8'hf0;
 end
 
 always #10 clk= ~clk;
@@ -44,6 +55,9 @@ initial begin
 end
 initial begin
   #1000 $finish;
+end
+
+endmodule
 end
 
 endmodule
